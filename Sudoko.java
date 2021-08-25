1b1d09403b3ad4aa1db1fb65cec216005076fe0a
tzur <Tzur.almog@gmail.com>
2021-08-25 15:47:58 -0400

    Added sample. Closing wend work. act list todo

--- Sudoko.java
+++ Sudoko.java
@@ -3,6 +3,8 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Sudoko extends JFrame{
+    final static int maxGap = 20;
+    static int cols = 9 ,rows = 9;
 
     public Sudoko(String name) {
         super(name);
@@ -11,7 +13,40 @@ public class Sudoko extends JFrame{
 
     public void addComponentsToPane(final Container pane) {
         final JPanel compsToExperiment = new JPanel();
-        compsToExperiment.setLayout(new GridLayout(3,3));
+        compsToExperiment.setLayout(new GridLayout(cols,rows));
+
+        // JButton b = new JButton("Just fake button");
+        // Dimension buttonSize = b.getPreferredSize();
+
+        // compsToExperiment.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 2.5)+maxGap, (int)(buttonSize.getHeight() * 3.5)+maxGap * 2));
+        // compsToExperiment.add(new SudokoButton("Button 1"));
+        for(int i = 0; i< rows; i++){
+            for(int j = 0; j< cols; j++){
+                compsToExperiment.add(new SudokoButton("Button X" + (i +1) + " Y" + (j +1)));
+            }
+        }
+        // compsToExperiment.add(new SudokoButton("Button 2"));
+        // compsToExperiment.add(new SudokoButton("Button 3"));
+        // compsToExperiment.add(new SudokoButton("Long-Named Button 4"));
+        // compsToExperiment.add(new SudokoButton("5"));
+
+        //Process the Apply gaps button press
+        // applyButton.addActionListener(new ActionListener(){
+        //     public void actionPerformed(ActionEvent e){
+        //         //Get the horizontal gap value
+        //         String horGap = (String)horGapComboBox.getSelectedItem();
+        //         //Get the vertical gap value
+        //         String verGap = (String)verGapComboBox.getSelectedItem();
+        //         //Set up the horizontal gap value
+        //         experimentLayout.setHgap(Integer.parseInt(horGap));
+        //         //Set up the vertical gap value
+        //         experimentLayout.setVgap(Integer.parseInt(verGap));
+        //         //Set up the layout of the buttons
+        //         experimentLayout.layoutContainer(compsToExperiment);
+        //     }
+        // });
+
+        pane.add(compsToExperiment, BorderLayout.NORTH);
     }
 
     private static void createAndShowGUI() {
---
+++ SudokoButton.java
@@ -0,0 +1,30 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class SudokoButton extends JButton{
+    public SudokoButton(String name) {
+        super(name);
+        // setAction(a);
+        // setResizable(false);
+        // addActionListener(new ActionListener(){
+        //     public void actionPerformed(ActionEvent e){
+        //         
+        //     }
+        // });
+        // super.addActionListener(new ActionListener(){
+        //     public void actionPerformed(ActionEvent e){
+        //         //Get the horizontal gap value
+                
+        //     }
+        // });
+    }
+
+    // @Override
+    // public void addActionListener(ActionListener l) {
+    //     // TODO Auto-generated method stub
+    //     System.out.println("I am " + this.getName());
+    //     this.setText("I am " + this.getName());
+    // }
+}
