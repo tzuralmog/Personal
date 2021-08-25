b73dfc4b27f9e085371abd544bcff8e112704ad5
tzur <Tzur.almog@gmail.com>
2021-08-24 20:18:00 -0400

    Tuesday End

--- Sudoko.java
+++ Sudoko.java
@@ -2,26 +2,26 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Sudoko {
+public class Sudoko extends JFrame{
+
+    public Sudoko(String name) {
+        super(name);
+        // setResizable(false);
+    }
+
+    public void addComponentsToPane(final Container pane) {
+        final JPanel compsToExperiment = new JPanel();
+        compsToExperiment.setLayout(new GridLayout(3,3));
+    }
 
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("FrameDemo");
+        Sudoko frame = new Sudoko("Sudoko");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create the menu bar.  Make it have a green background.
-        JMenuBar greenMenuBar = new JMenuBar();
-        greenMenuBar.setOpaque(true);
-        greenMenuBar.setBackground(new Color(154, 165, 127));
-        greenMenuBar.setPreferredSize(new Dimension(200, 20));
-
-        JLabel emptyLabel = new JLabel("Base");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.setJMenuBar(greenMenuBar);
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
+        // calls above function
+        frame.addComponentsToPane(frame.getContentPane());
         
-
         //Display the window.
         frame.pack();
         frame.setVisible(true);
